1fde582bf2c0c25a8ca1d673fb8838c1ad6f6c95
Mohamad Kayali <147993209+mohakay73@users.noreply.github.com>
2024-01-16 13:27:06 +0100

    admin list edit

--- data/admin-courses-list.js
+++ data/admin-courses-list.js
@@ -14,14 +14,16 @@ const initPage = async () => {
   cards.forEach((card) => {
     card.addEventListener('click', selectedCourse);
   });
+  console.log(courses);
 };
 
 const selectedCourse = (e) => {
   let courseId = 0;
-  if (e.target.location === `div`) {
+  if (e.target.location === 'div') {
     courseId = e.target.getAttribute('courseid');
   } else if (e.target.localName === 'span') {
     courseId = e.target.parentElement.getAttribute('courseid');
   }
+  console.log('course', e.target);
 };
 document.addEventListener('DOMContentLoaded', initPage);
